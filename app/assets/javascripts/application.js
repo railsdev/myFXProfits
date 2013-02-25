e3886ac1811fd524ad15b10a0d36796f78b13366
Farhan Munshi <farhanm995@gmail.com>
2013-02-25 17:59:19 -0500

    Hopefully fixes heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,17 @@
 //= require jquery_ujs
 //= require jquery.purr
 //= require best_in_place
+//= require bootstrap-affix
+//= require bootstrap-alert
+//= require bootstrap-button
+//= require bootstrap-carousel
+//= require bootstrap-collapse
+//= require bootstrap-dropdown
+//= require bootstrap-modal
+//= require bootstrap-popover
+//= require bootstrap-scrollspy
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-transition
+//= require bootstrap-typeahead
 //= require_tree .
