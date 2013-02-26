cf7fe190639a9142000a441c4d83e40f3361f362
Farhan Munshi <farhanm995@gmail.com>
2013-02-25 19:27:39 -0500

    fixing heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery.purr
-//= require best_in_place
 //= require bootstrap-affix
 //= require bootstrap-alert
 //= require bootstrap-button
@@ -27,4 +23,8 @@
 //= require bootstrap-tooltip
 //= require bootstrap-transition
 //= require bootstrap-typeahead
+//= require jquery
+//= require jquery_ujs
+//= require jquery.purr
+//= require best_in_place
 //= require_tree .
