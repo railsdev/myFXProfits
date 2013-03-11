3c428acac061fccb07845afb97cb089476e0b8a6
Farhan Munshi <farhanm995@gmail.com>
2013-03-11 17:46:47 -0400

    heroku

--- db/migrate/20130307214040_change_number_format_in_users.rb
+++
@@ -1,9 +0,0 @@
-class ChangeNumberFormatInUsers < ActiveRecord::Migration
- def up
-   change_column :users, :number, :integer
-  end
-
-  def down
-   change_column :users, :number, :string
-  end
-end
