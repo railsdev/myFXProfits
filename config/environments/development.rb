a5ebf2bf8be40e89f1f0085cd2b96fde4d35f772
Farhan Munshi <farhanm995@gmail.com>
2013-03-12 16:25:59 -0400

    FUCK:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,10 @@ Tradesite::Application.configure do
   config.assets.debug = true
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 
-    config.paperclip_defaults = {
+  config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
